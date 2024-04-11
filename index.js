0deb3da59fd140f40521e8d85c942ce80746e6ec
Pranav <pranavv2002@gmail.com>
2024-04-11 15:42:34 +0530

    Pilot

---
+++ index.js
@@ -0,0 +1,9 @@
+ReactDOM.render(<MainContent/>,  document.getElementById('root'));
+
+function MainContent() {
+    return(
+        <div className="container">
+            <h1>Hello</h1>
+        </div>
+    )
+}
