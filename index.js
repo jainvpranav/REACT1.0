042a9b5075fc8c8224ee1b619b0b8dc7670cb825
Pranav <pranavv2002@gmail.com>
2024-04-11 16:08:33 +0530

    Files Deleted

--- index.js
+++
@@ -1,9 +0,0 @@
-ReactDOM.render(<MainContent/>,  document.getElementById('root'));
-
-function MainContent() {
-    return(
-        <div className="container">
-            <h1>Hello</h1>
-        </div>
-    )
-}
